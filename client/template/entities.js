80012001e86191b1e92f721beb19b9c8bafd73a8
roaldmh <rmh@rmh.as>
2014-11-03 19:29:48 +0100

    Initial commit of folder structure and basic files

---
+++ client/template/entities.js
@@ -0,0 +1,19 @@
+var entityData = [
+    {
+        entityCode: 'RMH',
+        entityName: 'Roald Martin Hamnvik'
+    },
+    {
+        entityCode: "IH",
+        entityName: "Ingun Hamnvik"
+    },
+    {
+        entityCode: "LH",
+        entityName: "Linn Hamnvik"
+    }
+
+]
+
+Template.entities.helpers({
+    entities: entityData
+})
